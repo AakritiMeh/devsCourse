74837180d1ff20b50e76352807338c6e3f004897
Aakriti mehrotra <aakriti.meh.2004@gmail.com>
2024-06-09 14:45:36 +0530

    learning mongodb

---
+++ week 3/mongodb.js
@@ -0,0 +1,51 @@
+const express = require("express");
+
+const jwt = require("jsonwebtoken");
+const jwtPassword = "123456789";
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+
+// mongodb+srv://aakritimehrotra2022:AAKRITI%408oct@cluster0.gnp0cxr.mongodb.net/
+mongoose
+  .connect(
+    "mongodb+srv://aakritimehrotra2022:AAKRITI%408oct@cluster0.gnp0cxr.mongodb.net/newDb?retryWrites=true&w=majority&appName=Cluster0/"
+  )
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    passwd: String,
+  },
+  { collection: "users" }
+); // Specify collection name
+
+const User = mongoose.model("User", userSchema);
+
+const user = new User({
+  name: "aakriti",
+  email: "aakriti@gmail.com",
+  passwd: "12345",
+});
+
+user
+  .save()
+  .then(() => {
+    console.log("saved");
+  })
+  .catch((err) => {
+    console.log("not saved");
+  });
+
+//  uname- aakritimehrotra2022  passwd - AAKRITI@8oct
+
+//mongodb+srv:aakritimehrotra2022:AAKRITI@8oct@cluster0.qmt1szv.mongodb.net/
+
+// mongodb+srv://aakritimehrotra2022:AAKRITI%408oct@cluster0.gnp0cxr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/
