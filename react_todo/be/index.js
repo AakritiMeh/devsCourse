f0a4c3b11412a2a84ad76336b494255451997838
Aakriti mehrotra <aakriti.meh.2004@gmail.com>
2024-06-25 20:43:11 +0530

    re-initiated to-do application

---
+++ react_todo/be/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+
+app.use(express.json());
+
+app.post("/todo", function (req, res) {});
+
+app.get("/todos", function (req, res) {});
+
+app.put("/completed", function (req, res) {});
