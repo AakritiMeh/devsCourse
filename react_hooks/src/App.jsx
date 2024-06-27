1f2a6652366ea100e34f81a85e9cc2bbf30c404c
Aakriti mehrotra <aakriti.meh.2004@gmail.com>
2024-06-27 15:44:22 +0530

    reducing re-render

--- react_hooks/src/App.jsx
+++ react_hooks/src/App.jsx
@@ -1,13 +1,45 @@
-
+import { useState } from 'react'
 import './App.css'
+import PropTypes from "prop-types"
 
 function App() {
 
   return (
     <>
-     
+      <HeaderWithButton />
+      <br />
+      <Header title="aakriti"/>
     </>
   )
 }
+
+
+
+function HeaderWithButton(){
+  const [title,setTitle] =useState("Aakriti");
+  function updateTitle(){
+    setTitle("My name is "+ Math.random());
+  }
+  return (
+    <>
+      <button onClick={updateTitle}>update</button>
+      <Header title={title}></Header>
+    </>
+  )
+
+}
+
+function Header({title}){
+  return (
+    <>
+      {title}
+    </>
+  )
+}
+
+Header.propTypes={
+  title:PropTypes.string.isRequired
+}
+
 
 export default App
